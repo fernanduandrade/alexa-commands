21d8e115e6c4b52be0447ce3419ae2b8e3ec08ae
fernanduandrade <onanduandrade@gmail.com>
2022-12-07 07:25:26 -0300

    add: search lastest tweet about he4rt devs

--- He4rtTwitterSkill/Function.fs
+++ He4rtTwitterSkill/Function.fs
@@ -2,24 +2,30 @@ namespace He4rtTwitterSkill
 
 
 open Amazon.Lambda.Core
-
+open FSharp.Data
 open Alexa.NET.Request
 open Alexa.NET
-open Alexa.NET.Response
+open FunctionTypes
+open FSharp.Data.HttpRequestHeaders
+open System.Text.Json
+open System
 
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.Json.JsonSerializer>)>]
 ()
 
 
 type Function() =
-    /// <summary>
-    /// Função que retorna diz apenas Oi 😉
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
     member __.FunctionHandler (request: SkillRequest) (_: ILambdaContext) =
-        match request with
-        | _ -> ResponseBuilder.Tell("Oiii Fernando")
+        let token = Environment.GetEnvironmentVariable("BEARER")
+
+        let result = 
+            match Http.Request("https://api.twitter.com/2/tweets/search/recent?query=%40he4rtdevs&max_results=10&=", headers = [ Authorization $"Bearer {token}"]).Body with
+                | Text text -> text
+                | Binary binary -> failwithf "Expecting text, but got a binary response (%d bytes)" binary.Length
+               
+
+        let response = JsonSerializer.Deserialize<TweetResponse> result
+
+        ResponseBuilder.Tell($"o ultimo tweet sobre a heart foi: {response.data.Head.text}")
+        
---
+++ He4rtTwitterSkill/Types.fs
@@ -0,0 +1,22 @@
+﻿namespace FunctionTypes
+
+type TweetData =
+    {
+        edit_history_tweet_ids: string list;
+        id: string;
+        text: string
+    }
+
+type TweetMeta = 
+    {
+        newest_id: string;
+        oldest_id: string;
+        result_count: int;
+        next_token: string
+    }
+
+type TweetResponse = 
+    {
+        data: TweetData list;
+        meta: TweetMeta
+    }
