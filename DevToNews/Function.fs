981cfa4aa28036c22695f683d665dea825c911ab
fernanduandrade <onanduandrade@gmail.com>
2022-12-17 17:04:57 -0300

    fix: fetching to dev to

--- DevToNews/Function.fs
+++ DevToNews/Function.fs
@@ -7,6 +7,7 @@ open System
 open Alexa.NET
 open FSharp.Data
 open Alexa.NET.Request
+open System.Text
 
 [<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.Json.JsonSerializer>)>]
 ()
@@ -19,18 +20,24 @@ type Function() =
 
         let document = HtmlDocument.Load devToLastestFsharpPostUrl
 
-        let posts = document.CssSelect ".crayons-story__title"
+        let posts = document.CssSelect ".crayons-story"
 
         let postText selector (post: HtmlNode) =
             post.CssSelect(selector).[0].DirectInnerText().Trim()
 
+        let getReadTime selector (readTime: HtmlNode) =
+          readTime.CssSelect(selector).[0]
+
         let getLastestPost =
             posts
             |> Seq.map(fun post ->
-                post |> postText ".css-901oao .css-16my406 .r-poiln3 .r-bcqeeo .r-qvutc0"
-            ) |> Seq.take 1
+                post |> postText ".crayons-story__hidden-navigation-link",
+                post |> postText ".crayons-story__secondary",
+                post |> getReadTime ".crayons-story__save" |> postText ".crayons-story__tertiary"
+            )
+            |> Seq.take 3
+            |> Seq.map (fun (title, author, readTime) -> $"Título: {title}, Autor: {author}, Tempo de leitura {readTime}")
+            |> Seq.concat |> Seq.map string |> String.concat ""
 
-        for (title) in getLastestPost do
-            printf "\"%s" title
-
-        ResponseBuilder.Tell("Fernandinho es o melhor")
+        ResponseBuilder.Tell(getLastestPost)
+        
